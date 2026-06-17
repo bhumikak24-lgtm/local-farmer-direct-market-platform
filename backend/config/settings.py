23817876e559fb5d1f1d20820934110990314dda
akshitha <surugantiakshitha24@ifheindia.org>
2026-06-17 14:00:22 +0530

    Connect to Neon PostgreSQL database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -70,8 +70,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_1Ar8WbDiknCq',
+        'HOST': 'ep-fancy-shadow-ahfphail.c-3.us-east-1.aws.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
